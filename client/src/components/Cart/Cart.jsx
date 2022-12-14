0945fb81b8e4a2bb241fc7c0146464436834c960
Lydia Ego <egolydia97@gmail.com>
2022-12-14 11:31:32 +0100

    Feat: implement total price depending on product quantity

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import './Cart.scss'
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import { useSelector } from "react-redux"
+import { UPLOAD_URL } from '../../index'
 
 
 export default function Cart() {
     const products = useSelector(state => state.cart.products)
+
+    const totalPrice = () => {
+        let total = 0
+        products.forEach((item) => total += item.quantity * item.price)
+        return total.toFixed(2)
+    }
 
     return (
         <div className='cart'>
@@ -23,6 +30,7 @@ export default function Cart() {
             ))}
             <div className="total">
                 <span>SUBTOTAL</span>
+                <span>${totalPrice()}</span>
             </div>
             <button>PROCEED TO CHECKOUT</button>
             <div className="reset">Reset Cart</div>
