6be9f73addb9022191c608604d1a10ee7031adc5
Lydia Ego <egolydia97@gmail.com>
2022-12-14 11:29:54 +0100

    Feat: created cart component

---
+++ client/src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Cart.scss'
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+export default function Cart() {
+    return (
+        <div className='cart'>
+            <h1>Products in your cart</h1>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+            <div className="reset">Reset Cart</div>
+        </div>
+    )
+}
