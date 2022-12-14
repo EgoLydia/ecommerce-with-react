96eeac902240b3dcedd929c04a1158127a65695e
Lydia Ego <egolydia97@gmail.com>
2022-12-14 11:30:52 +0100

    Feat: use useSelector hook to reach into store js

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Cart.scss'
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { useSelector } from "react-redux"
+
+
 export default function Cart() {
+    const products = useSelector(state => state.cart.products)
+
     return (
         <div className='cart'>
             <h1>Products in your cart</h1>
+            {products?.map(item => (
+                <div className='item' key={item.id}>
+                    <img src={UPLOAD_URL + item.image} alt="" />
+                    <div className="details">
+                        <h1>{item.title}</h1>
+                        <p>{item.description?.substring(0, 100)}</p>
+                        <div className="price">{item.quantity} * ${item.price}</div>
+                    </div>
+                    <DeleteOutlineOutlinedIcon className='delete' />
+                </div>
+            ))}
+            <div className="total">
+                <span>SUBTOTAL</span>
             </div>
             <button>PROCEED TO CHECKOUT</button>
             <div className="reset">Reset Cart</div>
