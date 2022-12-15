a38709bea0f95fbaae8020c87040bf5768d73562
Lydia Ego <egolydia97@gmail.com>
2022-12-15 15:04:03 +0100

    Feat:handle proceed to checkout function and delete item method

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import './Cart.scss'
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { UPLOAD_URL } from '../../index'
-
+import { removeItem, resetCart } from '../../redux/cartReducer';
+import { loadStripe } from '@stripe/stripe-js';
+import { makeRequest } from '../../makeRequest'
 
 export default function Cart() {
     const products = useSelector(state => state.cart.products)
 
+    const dispatch = useDispatch()
     const totalPrice = () => {
         let total = 0
         products.forEach((item) => total += item.quantity * item.price)
         return total.toFixed(2)
     }
 
+    const stripePromise = loadStripe('pk_test_51MEsxtFIcmNWrQOrMMQbsWBjlWvMopzGbfUt3MuI72dl9f3VgqIRkcclDRjjO7TAvrrShXmuegL0UDrobUGlUpD800cEOgVui6');
+    const handlePayment = async () => {
+        try {
+            const stripe = await stripePromise
+            const response = await makeRequest.post("/orders", {
+                products
+            });
+
+            await stripe.redirectToCheckout({
+                sessionId: response.data.stripeSession.id
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <div className='cart'>
             <h1>Products in your cart</h1>
@@ -25,15 +43,15 @@ export default function Cart() {
                         <p>{item.description?.substring(0, 100)}</p>
                         <div className="price">{item.quantity} * ${item.price}</div>
                     </div>
-                    <DeleteOutlineOutlinedIcon className='delete' />
+                    <DeleteOutlineOutlinedIcon className='delete' onClick={() => dispatch(removeItem(item.id))} />
                 </div>
             ))}
             <div className="total">
                 <span>SUBTOTAL</span>
                 <span>${totalPrice()}</span>
             </div>
-            <button>PROCEED TO CHECKOUT</button>
-            <div className="reset">Reset Cart</div>
+            <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+            <div className="reset" onClick={() => dispatch(resetCart())}>Reset Cart</div>
         </div>
     )
 }
